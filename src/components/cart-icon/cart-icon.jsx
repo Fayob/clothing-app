4107401859f137f6e2ad09ce6a2dda7911aa0baf
= <adedejifavour6@gmail.com>
2022-11-19 08:14:26 +0100

    Add cart icon component

---
+++ src/components/cart-icon/cart-icon.jsx
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react'
+import { CartContext } from '../../context/cart.context'
+// import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
+
+import {CartIconContainer, ShoppingIcon, ItemCount} from './cart-icon.styles'
+
+const CartIcon = () => {
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext)
+
+  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen)
+  
+  return (
+    <CartIconContainer onClick={toggleIsCartOpen}>
+      <ShoppingIcon  />
+      <ItemCount>{cartCount}</ItemCount>
+    </CartIconContainer>
+  )
+}
+
+export default CartIcon
