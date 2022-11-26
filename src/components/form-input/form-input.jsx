c3200c2e6d1d8c09eb4cf2aa2eda03bddb0ea711
= <adedejifavour6@gmail.com>
2022-11-26 10:53:42 +0100

    Add form input

---
+++ src/components/form-input/form-input.jsx
@@ -0,0 +1,13 @@
+import {FormInputLabel, Group, Input} from './form-input.style.jsx'
+
+const FormInput = ({ label, ...otherProps }) => {
+  return (
+    <Group>
+      <Input className='form-input' {...otherProps} />
+      
+      {label && (<FormInputLabel shrink={otherProps.value.length}> {label} </FormInputLabel>)}
+    </Group>
+  )
+}
+
+export default FormInput
